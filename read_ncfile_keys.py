60438e8138ae2aafa4be5f904ec1248bf05c9349
爱垂钓的猫 <fishercat702@foxmail.com>
2021-05-14 22:20:07 +0800

    更新了垂直剖面绘图脚本

--- read_ncfile_keys.py
+++ read_ncfile_keys.py
@@ -6,6 +6,6 @@ from GetKeys import get_ncfile_keys
 
 #path:文件路径
 #num:间隔多少个换行
-path='D:\wrf_simulation\\2meic\wrfout_d03_2016-07-21_12_2meic'
-num=6
+path='D:\wrf_simulation\\2meic\wrfout_d03_2016-07-21_12_2meic'  #nc文件地址
+num=6   #一行多少个变量
 print(get_ncfile_keys(path,num))
